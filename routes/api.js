8b5a2eef3c9291cd37696869e081b821f91126c6
francisco-filho <francisco@capimgrosso.com>
2015-10-16 00:08:41 -0300

    add Router::api

---
+++ routes/api.js
@@ -0,0 +1,25 @@
+'use strict';
+var Connection = require('../lib/connection.js'),
+    conString = 'postgres://postgres:12345678@localhost/portal',
+    postgres = new Connection(conString),
+    router = require('express').Router();
+
+
+router.post('/query', function(req, res, next){
+    var queryStartTime = Date.now();
+
+    if (!req.body.sql){
+        return next();
+    }
+
+    postgres.query(req.body.sql, req.body.params || {}, function(err, result){
+        res.json({
+            fields: result.fields,
+            rows: result.rows,
+            rowCount: result.rowCount,
+            time: Date.now() - queryStartTime
+        });
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+'use strict';
+
 var http = require('http'),
     express = require('express'),
     app = require('express')(),
     bodyParser = require('body-parser'),
-    logger = require('morgan');
-var Connection = require('./lib/connection');
+    logger = require('morgan'),
+    api = require('./routes/api.js');
 
 app.set('port', 3030);
 
@@ -12,31 +14,11 @@ app.use(bodyParser.json({extended: false}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/public/'));
 
-app.post('/api/query', function(req, res, next){
-    var queryStartTime = Date.now();
+/** Routes **/
+app.use('/api', api);
 
-    if (!req.body.sql){
-        return next();
-    }
-
-    postgres.query(req.body.sql, req.body.params || {}, function(err, result){
-        res.json({
-            fields: result.fields,
-            rows: result.rows,
-            rowCount: result.rowCount,
-            time: Date.now() - queryStartTime
-        });
-    });
-
-});
-
-/** Database connection **/
-var conString = 'postgres://postgres:12345678@localhost/portal';
-
-var postgres = new Connection(conString);
-
-//Starting the server
-var server = http
+/** Starting the server **/
+http
     .createServer(app)
     .listen(app.get('port'), function(){
         console.info('Server Listening on port: ', app.get('port'));
